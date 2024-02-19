7e394f65f5f5d9e48dcf56d80433f5dd66ee05fb
Quarshie <asi37375@hotmail.com>
2024-02-19 19:48:46 +0000

    user prompts entered

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,67 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+{
+    type: 'input',
+    name: 'Title',
+    message: 'Whats the title of your project?'
 
+},
+{
+type: 'input',
+name: 'Description',
+message: 'Type in a description of your project:'
+},
+
+{
+    type:'input',
+    name: 'Installation',
+    message:'Enter Installion Process'
+
+},
+
+{
+    type:'input',
+    name: '',
+    message:''
+},
+
+{
+    type:'input',
+    name: 'Usage',
+    message:'Enter usage information'
+},
+
+{
+    type:'List',
+    name: 'License',
+    message:'Choose a License for your project'
+    choices: ['MIT, 'GPL 3.0', 'BSD 3', 'Eclipse Public License 2.0', 'None',]
+},
+
+{
+    type:'input',
+    name: 'Contribution guidelines:',
+    message:''
+},
+
+{
+    type:'input',
+    name: 'Tests',
+    message:'Enter test process'
+},
+
+{
+    type:'input',
+    name: 'Github',
+    message:'Enter Github username:'
+},
+
+{
+    type:'input',
+    name: 'Email',
+    message:'Enter email address:'
+},
 ];
 
 // function to write README file
